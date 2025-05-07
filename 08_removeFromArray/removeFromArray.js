47b7ffb1bb8095b9d73e793f50f848009bd955aa
rene <105558751+renefmelo@users.noreply.github.com>
2025-05-06 23:37:23 -0300

    done: 1 5 6 7 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arreio, argument1, argument2) {
+
+
+	for (let i = 0; i < arreio.length; i++) {
+		for (let j = 1; j < arguments.length; j++){
+
+			if (arreio.at(i) === arguments[j]) {
+				arreio.splice(i, 1);
+				i--
+			}
+		}
+	}
+
+	return arreio;
 };
 
 // Do not edit below this line
