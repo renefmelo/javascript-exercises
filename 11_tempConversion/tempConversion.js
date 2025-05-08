ef032aa4080c999e9d3b33a3ea701953039aa77f
rene <105558751+renefmelo@users.noreply.github.com>
2025-05-08 12:48:13 -0300

    done: 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+
+	let celsius = ((tempF) - 32) * 5/9;
+	if (Number.isInteger(celsius)) {
+		return celsius;
+	} else {
+		return Math.round(celsius * 1e1) / 1e1;
+	}
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+
+	let fahrenheit = ((tempC) * 9/5) + 32;
+	if (Number.isInteger(fahrenheit)) {
+		return fahrenheit;
+	} else {
+		return Math.round(fahrenheit * 1e1) / 1e1;
+	}
 };
 
 // Do not edit below this line
