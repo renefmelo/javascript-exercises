35aea15692b722f2f9d4b7393a516046536e550e
rene <105558751+renefmelo@users.noreply.github.com>
2025-05-27 19:26:58 -0300

    done: 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(numero) {
+	let fibo = [1, 1];
+	let i = 0;
+	numero = Number(numero);
+	if (numero === 0) {
+		return 0;
+	} else if (numero < 0) {
+		return "OOPS";
+	}
 
+	while (fibo.length < numero) {
+		fibo.push(fibo[i] + fibo[i + 1])
+		i++
+	}
+
+	return fibo[numero-1];
 };
 
 // Do not edit below this line
