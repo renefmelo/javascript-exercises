386dca3bd9f21da83ef1cc123c617068ad40b809
rene <105558751+renefmelo@users.noreply.github.com>
2025-05-27 14:30:51 -0300

    done: 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	//if (numbers.length === 0) {
+	//	return 0;
+	//}
+
+	return numbers.reduce((accumulator, currentElement) => accumulator + currentElement, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((accumulator, currentElement) => accumulator * currentElement, 1)
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let result = number;
+	if (number === 0 || number === 1) {
+		return 1;
+	} else {
+		while (number > 1) {
+			number--;
+			result = result * number;
+		}
+		return result;
+	}
 };
 
 // Do not edit below this line
