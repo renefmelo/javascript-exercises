20fda584754b4e6655fb38e022af0fe759201508
rene <105558751+renefmelo@users.noreply.github.com>
2025-05-27 15:15:43 -0300

    done: 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (sentence) {
+	let treatedSentence = sentence.toLowerCase().replace(/[^0-9a-z]/g, '');
+	let charArray = [...treatedSentence];
+	let joinedArray = charArray.reverse().join("");
+	if (joinedArray === treatedSentence) {
+		return true;
+	}
+	return false;
 };
 
 // Do not edit below this line
