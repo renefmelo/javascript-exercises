d867dcedad83d44eeff52d5fd7dff1476fe61b32
rene <105558751+renefmelo@users.noreply.github.com>
2025-05-28 13:37:12 -0300

    done: 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	let oldestAge = 0;
+	let oldestPerson = {};
+	people.forEach((person) => {
+		if (Object.hasOwn(person, "yearOfDeath")) {
+			let personAge = person.yearOfDeath - person.yearOfBirth;
+			if (personAge > oldestAge) {
+				oldestAge = personAge;
+				oldestPerson = person;
+			}
+		} else {
+			let yearNow = new Date().getFullYear();
+			let personAge = yearNow - person.yearOfBirth;
+			if (personAge > oldestAge) {
+				oldestAge = personAge;
+				oldestPerson = person;
+			}
+		}
+	})
+	return oldestPerson;
 };
 
 // Do not edit below this line
