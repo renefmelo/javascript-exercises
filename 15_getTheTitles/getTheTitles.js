1f4e1d22de8c7375c21b009f0b12cc64932f6af6
rene <105558751+renefmelo@users.noreply.github.com>
2025-05-28 12:40:13 -0300

    done: 15_getTheTitles

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,5 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles = function(arr) {
+	return arr.map((book) => book.title)// lembrar de usar map
+}; 
 // Do not edit below this line
 module.exports = getTheTitles;
